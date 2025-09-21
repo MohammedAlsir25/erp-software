91606bb63621c144799ca85343f167247a2529b0
dreambooy9 <dreambooy9@gmail.com>
2025-09-21 06:25:07 +0000

    Changes before Firebase Studio auto-run

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,24 +1,9 @@
-{pkgs}: {
-  channel = "stable-24.05";
-  packages = [
-    pkgs.flutter,
-    pkgs.flutter319,
-    pkgs.jdk17,
-    pkgs.unzip
-  ];
-  idx.extensions = [
-    
-  ];
-  idx.previews = {
-    previews = {
-      web = {
-        command = [
-          "flutter",
-          "run",
-          "-d",
-          "windows"
-        ];
-      };
-    };
-  };
+{ pkgs, ... }: {
+	channel = "stable-23.11";
+	packages = [
+		pkgs.flutter
+	];
+	idx.extensions = [
+		"dart-code.flutter"
+	];
 }
